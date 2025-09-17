c5da4ab4debf348720c97fcd1802a01ae91ba06c
DexLovecraft <101021602+DexLovecraft@users.noreply.github.com>
2025-09-17 13:18:47 +0200

    essai fix stage/prod difference

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,29 @@ const cleanCSS = require("gulp-clean-css");
 const terser = require("gulp-terser");
 const htmlmin = require("gulp-htmlmin");
 
-// Minify CSS
 function minifyCSS() {
-  return src("src/**/*.css")
+  return src("src/**/*.css", { base: "src" })
     .pipe(cleanCSS())
     .pipe(dest("dist"));
 }
 
-// Minify JS
 function minifyJS() {
-  return src("src/**/*.js")
+  return src("src/**/*.js", { base: "src" })
     .pipe(terser())
     .pipe(dest("dist"));
 }
 
-// Minify HTML
 function minifyHTML() {
-  return src("src/**/*.html")
+  return src("src/**/*.html", { base: "src" })
     .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
     .pipe(dest("dist"));
 }
 
-// Copier autres fichiers (images, icônes, etc.)
 function copyAssets() {
-  return src("src/**/*.{png,jpg,jpeg,gif,svg,ico,webp,mp3,pdf}", { encoding: false })
+  return src("src/**/*.{png,jpg,jpeg,gif,svg,ico,webp,mp3,pdf}", { encoding: false, base: "src" })
     .pipe(dest("dist"));
 }
+
 
 exports.build = series(
   parallel(minifyCSS, minifyJS, minifyHTML, copyAssets),
